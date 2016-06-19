40b9c89eb9586151e61003a23b3b25891bf6f668
cly753 <cly753@gmail.com>
2016-06-19 17:50:47 +0200

    clean up constants.h

--- graph500-2.1.4/mpi/src/constants.h
+++ graph500-2.1.4/mpi/src/constants.h
@@ -1,14 +1,9 @@
 #ifndef MPI_CONSTANTS_H
 #define MPI_CONSTANTS_H
 
-#ifndef SKIP_VALIDATE
-// #define SKIP_VALIDATE
-#endif
-
-// #define BOTTOM_UP
-
 #define FILTER_EDGE
 // #define FILTER_ZERO_DEGREE
+// #define PUT_RECOVER_ZERO_DEGREE_IN_VALIDATION
 
 // #define SHOWDEBUG
 #define SHOWTIMER
